5a5e212cd3539ead6eb1f1d10a2f0a1f7c5073e1
hebertspitzer <hebertsp03@outlook.com>
2022-10-05 21:06:51 -0300

    Add custumersPage.h

---
+++ CustumersPage.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int CustomersPage(){
+    //setlocale permite usar acentos para isso precisa inportar a biblioteca "<locale.h>" em cima
+    setlocale(LC_ALL, "Portuguese");
+
+    //var usada para selecionar o item do menu
+    int select;
+
+do{
+    printf("-----------------Página de clientes-----------------\n");
+    printf("1 - Ver todos os clientes\n");
+    printf("2 - adicionar novo cliente\n");
+    printf("3 - remover um cliente\n");
+    printf("4 - editar um cliente\n");
+    printf("0 - Voltar para o menu\n");
+
+    printf("Digite o número da Função desejada:");
+    scanf("%d",&select);
+
+    //switch case usado para selecionar o item do menu
+    switch (select)
+    {
+    case 1:
+        system("cls");
+        printf("Ver todos os clientes\n");
+        break;
+    case 2:
+        system("cls");
+        printf("adicionar novo cliente\n");
+        break;
+    case 3:
+        system("cls");
+        printf("remover um cliente\n");
+        break;
+    case 4:
+        system("cls");
+        printf("editar um cliente\n");
+        break;
+    default:
+        system("cls");
+        break;
+    }
+    if(select < 0 || select > 4){
+        printf("O número da função não e válido\n");
+    }
+
+}while (select != 0);
+
+}
