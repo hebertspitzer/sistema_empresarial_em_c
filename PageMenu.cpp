6ae1b7c232b83d7003c427954e9d07bd9de022e0
hebertspitzer <hebertsp03@outlook.com>
2022-10-05 21:09:05 -0300

    Adjusted PageMenu.cpp

---
+++ PageMenu.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "CustumersPage.h"
+
+int main(){
+    //setlocale permite usar acentos para isso precisa inportar a biblioteca "<locale.h>" em cima
+    setlocale(LC_ALL, "Portuguese");
+
+    //var usada para selecionar o item do menu
+    int select;
+
+do{
+    printf("--------------------Páginas--------------------\n");
+    printf("\t\t1 - Clientes\n");
+    printf("\t\t2 - Marketing\n");
+    printf("\t\t3 - Financeiro\n");
+    printf("\t\t4 - Tarefas\n");
+    printf("\t\t5 - Possíveis clientes\n");
+    printf("\t\t0 - Voltar para login\n");
+    printf("-----------------------------------------------\n");
+
+    printf("Digite o número da página desejada:");
+    scanf("%d",&select);
+
+    //switch case usado para selecionar o item do menu
+    switch (select)
+    {
+    case 1:
+        system("cls");
+        CustomersPage();
+        break;
+    case 2:
+        system("cls");
+        printf("\t\tPágina de Marketing selecionada\n");
+        break;
+    case 3:
+        system("cls");
+        printf("\t\tPágina de Financeiro selecionada\n");
+        break;
+    case 4:
+        system("cls");
+        printf("\t\tPágina de Tarefas selecionada\n");
+        break;
+    case 5:
+        system("cls");
+        printf("\t\tPágina de Possíveis clientes selecionada\n");
+        break;
+    default:
+        system("cls");
+        break;
+    }
+    if(select < 0 || select > 5){
+        printf("\t\tO número da página não e válido\n");
+    }
+
+}while (select != 0);
+
+}
