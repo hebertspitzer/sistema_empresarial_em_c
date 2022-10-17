55845103345a47fb85ceb7ab5d4e02c145942bf1
hebertspitzer <hebertsp03@outlook.com>
2022-10-16 23:20:48 -0300

    Adicionado pagina de tarefas

--- TaskPage.h
+++ TaskPage.h
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <locale.h>
 
+typedef struct {
+	int prazo;
+	char tarefa[101];
+}Tarefas;
+
+void CriaTarefa();
+void ListaTarefas();
+
 int TaskPage(){
+	
     //setlocale permite usar acentos para isso precisa importar a biblioteca "<locale.h>" em cima
     setlocale(LC_ALL, "Portuguese");
 
@@ -26,20 +35,20 @@ do{
     switch (select)
     {
     case 1:
-        system("cls");
-        printf("ver todas as tarefas\n");
+    	system("cls");
+        ListaTarefas();
         break;
     case 2:
-        system("cls");
-        printf("adicionar uma nova tarefa\n");
+    	system("cls");
+        CriaTarefa();
         break;
     case 3:
         system("cls");
-        printf("remover uma tarefa\n");
+        printf("remover um cliente\n");
         break;
     case 4:
         system("cls");
-        printf("editar uma tarefa\n");
+        printf("editar um cliente\n");
         break;
     default:
         system("cls");
@@ -52,3 +61,41 @@ do{
 }while (select != 0);
 
 }
+
+void CriaTarefa() {
+	FILE *farq = fopen("tarefas.txt", "a");
+	if(farq == NULL){
+		exit(0);
+	}
+	Tarefas ttarefas;
+	
+	printf("Digite o prazo da tarefa em horas: ");
+	scanf( "%d", &ttarefas.prazo);
+	fflush(stdin);
+	printf("Digite a tarefa: ");
+	scanf( "%100s", ttarefas.tarefa);
+	fflush(stdin);
+	
+	printf("\t------Tarefa adicionada com sucesso------\n");
+	printf("Prazo: %d horas\n",ttarefas.prazo);
+	printf("Tarefa: %s\n",ttarefas.tarefa);
+	
+	fprintf(farq, "\n%d %s", ttarefas.prazo, ttarefas.tarefa);
+	fclose(farq);
+}
+
+void ListaTarefas(){
+	FILE *farq = fopen("tarefas.txt", "r+b");
+	if(farq == NULL){
+		exit(0);
+	}
+	fseek(farq, 0, SEEK_SET);
+	while(!feof(farq)){
+		Tarefas ttarefas;
+		fscanf(farq, "%d %s", &ttarefas.prazo, ttarefas.tarefa);
+		printf("Prazo:%d horas\nTarefa:%s\n--------------------------------\n", ttarefas.prazo, ttarefas.tarefa);
+	}
+	fclose(farq);
+	//nao consegue ler nome com espaço
+}
+
