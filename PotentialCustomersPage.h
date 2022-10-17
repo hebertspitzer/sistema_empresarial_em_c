bb9ac17d14262d3fbc93505dc022d030faf5b322
hebertspitzer <hebertsp03@outlook.com>
2022-10-16 23:08:54 -0300

    Adicionado pagina de possiveis clientes

--- PotentialCustomersPage.h
+++ PotentialCustomersPage.h
@@ -1,8 +1,18 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 
+typedef struct {
+	char nome[26];
+	char email[31];
+	char telefone[12];
+}PossivelCliente;
+
+void CriaPossivelCliente();
+void ListaPossiveisclientes();
+
 int PotentialCustomersPage(){
+	
     //setlocale permite usar acentos para isso precisa importar a biblioteca "<locale.h>" em cima
     setlocale(LC_ALL, "Portuguese");
 
@@ -26,12 +36,12 @@ do{
     switch (select)
     {
     case 1:
-        system("cls");
-        printf("ver todos os possiveis clientes\n");
+    	system("cls");
+        ListaPossiveisclientes();
         break;
     case 2:
-        system("cls");
-        printf("adicionar um novo possivel cliente\n");
+    	system("cls");
+        CriaPossivelCliente();
         break;
     case 3:
         system("cls");
@@ -52,3 +62,46 @@ do{
 }while (select != 0);
 
 }
+
+void CriaPossivelCliente() {
+	FILE *farq = fopen("possiveisClientes.txt", "a");
+	if(farq == NULL){
+		exit(0);
+	}
+	PossivelCliente tpossivelCliente;
+	
+	printf("Digite o nome do possivel cliente: ");
+	scanf( "%25s", tpossivelCliente.nome);
+	fflush(stdin);
+	printf("Digite o email do possivel cliente: ");
+	scanf( "%30s", tpossivelCliente.email);
+	fflush(stdin);
+	printf("Digite o telefone do possivel cliente: ");
+	scanf( "%11s", tpossivelCliente.telefone);
+	fflush(stdin);
+	system("cls");
+	
+	printf("\t------possivel cliente adicionado com sucesso------\n");
+	printf("Nome: %s\n",tpossivelCliente.nome);
+	printf("Email: %s\n",tpossivelCliente.email);
+	printf("Telefone: %s\n\n",tpossivelCliente.telefone);
+	
+	fprintf(farq, "\n%s %s %s", tpossivelCliente.nome, tpossivelCliente.email, tpossivelCliente.telefone);
+	fclose(farq);
+}
+
+void ListaPossiveisclientes(){
+	FILE *farq = fopen("possiveisClientes.txt", "r+b");
+	if(farq == NULL){
+		exit(0);
+	}
+	fseek(farq, 0, SEEK_SET);
+	while(!feof(farq)){
+		PossivelCliente tpossivelCliente;
+		fscanf(farq, "%s %s %s", tpossivelCliente.nome, tpossivelCliente.email, tpossivelCliente.telefone);
+		printf("Nome:%s \nEmail:%s \nTelefone:%s\n--------------------------------\n", tpossivelCliente.nome, tpossivelCliente.email, tpossivelCliente.telefone);
+	}
+	fclose(farq);
+	//nao consegue ler nome com espaço
+}
+
