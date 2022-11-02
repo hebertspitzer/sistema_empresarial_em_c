69e71c21e8b86d1edc0760b403ba9cb2573df62d
hebertspitzer <hebertsp03@outlook.com>
2022-11-02 19:15:05 -0300

    Adicionado comentarios a FinancierPage

--- arquivos_h/FinancierPage.h
+++ arquivos_h/FinancierPage.h
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//declarando uma struct
 typedef struct {
 	float valorEnt;
 	float valorSaido;
 }Financas;
 
+//declarando as funções
 void CriaFinancas();
 void ListaFinancas();
 
@@ -15,6 +17,7 @@ int FinancierPage(){
     //setlocale permite usar acentos para isso precisa importar a biblioteca "<locale.h>" em cima
     setlocale(LC_ALL, "Portuguese");
     
+    //configurando para aceitar virgula no float
     setlocale(LC_NUMERIC, "pt_BR");
 
     //var usada para selecionar o item do menu
@@ -29,7 +32,7 @@ do{
     printf("0 - Voltar para o menu\n");
     printf("-----------------------------------------------\n");
 
-
+	//entrada do numero da função
     printf("Digite o número da Função desejada:");
     scanf("%d",&select);
 
@@ -56,24 +59,37 @@ do{
         system("cls");
         break;
     }
+    //teste para se o usuario inserir numero menor ou maior do que o permitido
     if(select < 0 || select > 4){
         printf("O número da função não e válido\n");
     }
-
+//se o numero selecionado for zero o sistema finaliza
 }while (select != 0);
 
+//fim da função principal main
 }
 
+
+//criação da função CriaFinancas
 void CriaFinancas() {
+	
+	//abrindo o arquivo
 	FILE *farq = fopen("arquivos_txt/financas.txt", "a");
+	
+	//verificando se o arquivo abriu corretamente
 	if(farq == NULL){
 		exit(0);
 	}
+	
+	//declaração das variaveis
 	Financas tfinancas;
 	
+	//entrada do valor entrado
 	printf("Digite o valor entrado: ");
 	scanf( "%f", &tfinancas.valorEnt);
 	fflush(stdin);
+	
+	//entrada do valor saido
 	printf("Digite o valor saido: ");
 	scanf( "%f", &tfinancas.valorSaido);
 	fflush(stdin);
@@ -82,21 +98,41 @@ void CriaFinancas() {
 	printf("valor entrado: %f\n",tfinancas.valorEnt);
 	printf("valor saido: %f\n",tfinancas.valorSaido);
 	
+	//salvando as informações no arquivo txt
 	fprintf(farq, "\n%f %f", tfinancas.valorEnt, tfinancas.valorSaido);
+	
+	//fechando o arquivo
 	fclose(farq);
 }
 
+//criação da função ListaFinancas
 void ListaFinancas(){
+	
+	//abrindo o arquivo
 	FILE *farq = fopen("arquivos_txt/financas.txt", "r+b");
+	
+	//verificando se o arquivo abriu corretamente
 	if(farq == NULL){
 		exit(0);
 	}
+	
+	//apontando no inicio do arquivo
 	fseek(farq, 0, SEEK_SET);
+	
+	//lendo linha por linha do arquivo ate o fim dele
 	while(!feof(farq)){
+		
+		//declaração das variaveis
 		Financas tfinancas;
+		
+		//lendo as infos do arquivo e salvando na struct tfinancas
 		fscanf(farq, "%f %f", &tfinancas.valorEnt, &tfinancas.valorSaido);
+		
+		//imprimindo as infos da struct tfinancas
 		printf("Valor entrado: %f \nValor saido: %f\n--------------------------------\n", tfinancas.valorEnt, tfinancas.valorSaido);
 	}
+	
+	//fechando o arquivo
 	fclose(farq);
 }
 
