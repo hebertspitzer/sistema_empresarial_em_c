742f2cc83a0dd6c3cd9b56576585c5c3ecd95ad6
hebertspitzer <hebertsp03@outlook.com>
2022-11-02 18:46:25 -0300

    Adicionado comentarios ao codigo

--- login.c
+++ login.c
@@ -5,17 +5,23 @@
 #include <string.h>
 #include "arquivos_h/PageMenu.h"
 
+//declarando uma struct
 typedef struct {
 	char email[30];
 	char nome[26];
 	char senha[20];
 }Conta;
 
+//declarando as funções
 void FazerLogin();
 void CriaConta();
 
+
 int main(){
+	//ajustando para aceitar acentos
     setlocale(LC_ALL, "Portuguese");
+    
+    //declaração da variavel de seleção de função
     int select;
 
 do{
@@ -24,8 +30,10 @@ do{
     printf("3 - Recuperar senha\n");
     printf("0 - Finalizar o sistema\n");
 
+	//entrada do numero da função
     printf("Digite o numero da opcao desejada:");
     scanf("%d",&select);
+    
     switch (select)
     {
     case 1:
@@ -45,11 +53,14 @@ do{
     default:
         break;
     }
+    
+    //teste para se o usuario inserir numero menor ou maior do que o permitido
     if(select < 0 || select > 3){
     	system("cls");
         printf("O número da opcao não e valido\n");
     }
-
+    
+//se o numero selecionado for zero o sistema finaliza
 }while (select != 0);
 }
 
@@ -82,30 +93,46 @@ void CriaConta(){
 }
 
 void FazerLogin(){
+	//declaração das variaveis que serão utilizadas
 	char email[30], senha[30];
-	int verificacao=0, testeSenha, testeEmail;
-	char temail[30];
+	int testeSenha, testeEmail;
 	
+	//entrada de dados do login
 	printf("Insira o seu email: ");
 	scanf( "%29s", email);
 	fflush(stdin);
+	
 	printf("Insira a sua senha: ");
 	scanf( "%19s", senha);
 	fflush(stdin);
 	
+	//abrindo o arquivo
 	FILE *farq = fopen("arquivos_txt/conta.txt", "r+b");
+	
+	//verificando se o arquivo abriu corretamente
 	if(farq == NULL){
 		exit(0);
 	}
+	
+	//apontando no inicio do arquivo
 	fseek(farq, 0, SEEK_SET);
+	
+	//lendo linha por linha do arquivo ate o fim dele
 	while(!feof(farq)){
+		
 		Conta tconta;
+		
 		fscanf(farq, "%s %s %s", tconta.email, tconta.nome, tconta.senha);
+		
+		//testando se o email e senha digitado pelo usuario é o mesmo que esta salvo na linha
 		testeEmail = strncmp(tconta.email, email, 29);
 		testeSenha = strncmp(tconta.senha, senha, 19);
 	}
+	
+	//fechando o arquivo
 	fclose(farq);
 	
+	//teste para verificar se o email e senha estão corretos
 	if(testeEmail>0 || testeEmail<0 || testeSenha>0 || testeSenha<0){
 		system("cls");
 		printf("*********Os dados inseridos são invalidos*********");
