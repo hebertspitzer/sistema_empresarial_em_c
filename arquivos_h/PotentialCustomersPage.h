43471a974ebcc2d333722b1e4665c192bfcec159
hebertspitzer <hebertsp03@outlook.com>
2022-11-02 19:24:03 -0300

    Adicionado comentarios a PotentialCustomersPage

--- arquivos_h/PotentialCustomersPage.h
+++ arquivos_h/PotentialCustomersPage.h
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <locale.h>
 
+//declarando uma struct
 typedef struct {
 	char nome[26];
 	char email[31];
 	char telefone[12];
 }PossivelCliente;
 
+//declarando as funções
 void CriaPossivelCliente();
 void ListaPossiveisclientes();
 
@@ -28,7 +30,7 @@ do{
     printf("0 - Voltar para o menu\n");
     printf("-----------------------------------------------\n");
 
-
+	//entrada do numero da função
     printf("Digite o número da Função desejada:");
     scanf("%d",&select);
 
@@ -57,25 +59,37 @@ do{
         system("cls");
         break;
     }
+    //teste para se o usuario inserir numero menor ou maior do que o permitido
     if(select < 0 || select > 4){
         printf("O número da função não e válido\n");
     }
-
+//se o numero selecionado for zero o sistema finaliza
 }while (select != 0);
 
+//fim da função principal main
 }
 
+//criação da função CriaFinancas
 void CriaPossivelCliente() {
+	
+	//abrindo o arquivo
 	FILE *farq = fopen("arquivos_txt/possiveisClientes.txt", "a");
+	
+	//verificando se o arquivo abriu corretamente
 	if(farq == NULL){
 		exit(0);
 	}
+	
+	//declaração das variaveis
 	PossivelCliente tpossivelCliente;
 	char tstring[25];
+	
+	//entrada do nome do possivel cliente
 	printf("Digite o nome do possivel cliente: ");
 	scanf( "%25[^\n]s", tpossivelCliente.nome);
 	fflush(stdin);
 	
+	//tratamento do nome do possivel cliente alterando o espaço por "_" e salvando em outra string
 	for(int i = 0; i < 25; i++) {
         if (tpossivelCliente.nome[i] == ' '){
         	tstring[i] = '_';
@@ -86,9 +100,12 @@ void CriaPossivelCliente() {
 		}
     }
     
+    //entrada do email do possivel cliente
 	printf("Digite o email do possivel cliente: ");
 	scanf( "%30s", tpossivelCliente.email);
 	fflush(stdin);
+	
+	//entrada do telefone do possivel cliente
 	printf("Digite o telefone do possivel cliente: ");
 	scanf( "%11s", tpossivelCliente.telefone);
 	fflush(stdin);
@@ -99,23 +116,37 @@ void CriaPossivelCliente() {
 	printf("Email: %s\n",tpossivelCliente.email);
 	printf("Telefone: %s\n\n",tpossivelCliente.telefone);
 	
+	//salvando as infos do cliente no arquivo txt
 	fprintf(farq, "\n%s %s %s", tstring, tpossivelCliente.email, tpossivelCliente.telefone);
+	
+	//fechando o arquivo
 	fclose(farq);
 }
 
 void ListaPossiveisclientes(){
+	
+	//abrindo o arquivo
 	FILE *farq = fopen("arquivos_txt/possiveisClientes.txt", "r+b");
+	
+	//verificando se o arquivo abriu corretamente
 	if(farq == NULL){
 		exit(0);
 	}
 	
-	char tstring[25];
+	//apontando no inicio do arquivo
 	fseek(farq, 0, SEEK_SET);
+	
+	//lendo linha por linha do arquivo ate o fim dele
 	while(!feof(farq)){
+		
+		//declaração das variaveis
+		char tstring[25];
 		PossivelCliente tpossivelCliente;
 		
+		//lendo as infos do arquivo e salvando na struct tpossivelCliente
 		fscanf(farq, "%s %s %s", tpossivelCliente.nome, tpossivelCliente.email, tpossivelCliente.telefone);
 		
+		//tratando o nome do cliente, trocando o "_" por espaço
 		for(int i = 0; i < 25; i++) {
         	if (tpossivelCliente.nome[i] == '_'){
         		tstring[i] = ' ';
@@ -124,9 +155,10 @@ void ListaPossiveisclientes(){
 			}
     	}
 		
+		//imprimindo as infos da struct tpossivelCliente
 		printf("Nome:%s \nEmail:%s \nTelefone:%s\n--------------------------------\n", tstring, tpossivelCliente.email, tpossivelCliente.telefone);
 	}
+	//fechando o arquivo
 	fclose(farq);
-	//nao consegue ler nome com espaço
 }
 
